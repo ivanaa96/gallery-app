2a7c8e6f453dea728a8ff5895b2ed63b96095aa3
ivanaa96 <ivana.stanic1@hotmail.com>
2022-04-17 14:53:46 +0200

    Forgot to commit index file in store folder

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/slice";
+import galleryReducer from "./gallery/slice";
 
 export default configureStore({
-	reducer: { user: userReducer },
+	reducer: { user: userReducer, gallery: galleryReducer },
 });
