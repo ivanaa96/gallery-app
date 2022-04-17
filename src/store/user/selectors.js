f2bbdeeb1377d91fd278a77145f259bbcac0a362
ivanaa96 <ivana.stanic1@hotmail.com>
2022-04-17 16:55:47 +0200

    Added isAuth in user store

--- src/store/user/selectors.js
+++ src/store/user/selectors.js
@@ -1,1 +1,2 @@
 export const userSelector = (state) => state.user.user;
+export const isAuthenticated = (state) => state.user.isAuthenticated;
--- src/store/user/slice.js
+++ src/store/user/slice.js
@@ -4,13 +4,16 @@ export const userSlice = createSlice({
 	name: "user",
 	initialState: {
 		user: {},
+		isAuthenticated: false,
 	},
 	reducers: {
 		setUser(state, action) {
 			state.user = action.payload;
+			state.isAuthenticated = true;
 		},
 		removeUser(state) {
 			state.user = {};
+			state.isAuthenticated = false;
 		},
 	},
 });
