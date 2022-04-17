590e64b5054119642f4464d223bbe15954e5e82b
ivanaa96 <ivana.stanic1@hotmail.com>
2022-04-17 08:26:14 +0200

    PrivateRoute and GuestRoute

---
+++ src/components/shared/GuestRoute.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Redirect, Route } from "react-router";
+
+function GuestRoute({ children, ...props }) {
+	const token = localStorage.getItem("token");
+	const isGuest = !token;
+
+	return (
+		<Route {...props}>{isGuest ? children : <Redirect to="/posts" />}</Route>
+	);
+}
+
+export default GuestRoute;
